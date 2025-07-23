d12e494c18e4184cad5030c03347765fa1f0a1d1
upraityharshit <upraityharshit@gmail.com>
2025-07-23 16:15:52 +0530

    230720251615

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -3,8 +3,22 @@ const nav2 = document.getElementById("nav2");
 
     btn.onclick = () =>{
         if(nav2.style.top == "0px")
-            nav2.style.top = "60px";
+            nav2.style.top = "50px";
         else
             nav2.style.top = "0px";
     }
+
+function toggleMenu() {
+    const menu = document.getElementById('profile-menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
+
+// Close the menu if clicked outside
+document.addEventListener('click', function (event) {
+    const menu = document.getElementById('profile-menu');
+    const icon = document.querySelector('.circle-icon');
+    if (!icon.contains(event.target) && !menu.contains(event.target)) {
+        menu.style.display = 'none';
+    }
+});
   
